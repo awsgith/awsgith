ece65ed185873032c4987384f649111acef4d8be
awsgith <158044757+awsgith@users.noreply.github.com>
2024-02-28 02:40:43 +0530

    Update copyfile.sh

--- copyfile.sh
+++ copyfile.sh
@@ -1,3 +1,3 @@
-cp /usr/local/jenkins-service/workspace/shell-pipeline/wget.sh /tmp/19c/
+sudo cp /usr/local/jenkins-service/workspace/shell-pipeline/wget.sh /tmp/19c/
 cd /tmp/
 sudo chown -R oracle:oinstall 19c
